fa8b13bc82fa1a2930a861a60202082bba4140dd
feiyu <felixchenfy@gmail.com>
2019-12-09 17:22:25 -0600

    Organized the code of rviz marker.

--- utils/lib_ros_marker.py
+++ utils/lib_ros_marker.py
@@ -1,96 +1,111 @@
 #!/usr/bin/env python
 '''
-An example code of creating and publishing a marker.
+A wrapper class for drawing dots or links on Rviz.
+
+Unit test:
+    python utils/lib_ros_marker.py
+    roslaunch ros_openpose_rgbd run_rviz.launch 
 '''
 
 from visualization_msgs.msg import Marker
 from std_msgs.msg import Header
 import rospy
+import copy
+import math
+from geometry_msgs.msg import Point
 
 
-def get_marker(marker_type):
-    '''
-    Usage:
-        MARKER_LINE = get_marker("line")
-        MARKER_POINT = get_marker("point")
-    '''
-    if marker_type == "point":
-        USE_POINT = True
-    elif marker_type == "line":
-        USE_POINT = False
-    else:
-        assert(False)
-    marker = Marker()
-    marker.header = Header()
-    marker.header.frame_id = "/map"
-    marker.header.stamp = rospy.Time.now()
-    if USE_POINT:
+class MarkerDrawer(object):
+    def __init__(self, frame_id="map", topic_name="visualization_marker"):
+
+        self._pub = rospy.Publisher(topic_name, Marker, queue_size=10)
+        self._MARKER_TEMPLATE = self._create_template_marker()
+        self._MARKER_TEMPLATE.header.frame_id = frame_id
+
+    def draw_dot(self, id, xyz, size=0.1, lifetime=-1):
+        marker = copy.deepcopy(self._MARKER_TEMPLATE)
+        marker.id = id
         marker.type = marker.SPHERE
-    else:
+        marker.header.stamp = rospy.Time.now()
+        marker.pose.position.x = xyz[0]
+        marker.pose.position.y = xyz[1]
+        marker.pose.position.z = xyz[2]
+        marker.scale.x = size
+        marker.scale.y = size
+        marker.scale.z = size
+        if lifetime > 0:
+            marker.lifetime = rospy.Duration(lifetime)
+        self._pub.publish(marker)
+
+    def draw_link(self, id, xyz1, xyz2, size=0.1, lifetime=-1):
+        marker = copy.deepcopy(self._MARKER_TEMPLATE)
+        marker.id = id
         marker.type = marker.LINE_LIST
-    marker.action = marker.ADD
-    marker.pose.position.x = 0
-    marker.pose.position.y = 0
-    marker.pose.position.z = 0
-    marker.pose.orientation.x = 0.0
-    marker.pose.orientation.y = 0.0
-    marker.pose.orientation.z = 0.0
-    marker.pose.orientation.w = 1.0
-    marker.scale.x = 0.1
-    marker.scale.y = 0.1 if USE_POINT else 0
-    marker.scale.z = 0.1 if USE_POINT else 0
-    marker.color.a = 1.0
-    marker.color.r = 0.0
-    marker.color.g = 1.0
-    marker.color.b = 0.0
-    return marker
+        marker.header.stamp = rospy.Time.now()
+        marker.points = [Point(*xyz1), Point(*xyz2)]
+        marker.scale.x = size
+        if lifetime > 0:
+            marker.lifetime = rospy.Duration(lifetime)
+        self._pub.publish(marker)
+
+    def _create_template_marker(self):
+        marker = Marker()
+        marker.header = Header()
+        marker.action = marker.ADD
+        marker.pose.position.x = 0.0
+        marker.pose.position.y = 0.0
+        marker.pose.position.z = 0.0
+        marker.pose.orientation.x = 0.0
+        marker.pose.orientation.y = 0.0
+        marker.pose.orientation.z = 0.0
+        marker.pose.orientation.w = 1.0
+        marker.scale.x = 0
+        marker.scale.y = 0
+        marker.scale.z = 0
+        marker.color.a = 1.0
+        marker.color.r = 0.0
+        marker.color.g = 1.0
+        marker.color.b = 0.0
+        return marker
 
 
 if __name__ == '__main__':
     ''' An example of draw marker on rviz. '''
 
-    import copy
-    import math
-    from geometry_msgs.msg import Point
-
     rospy.init_node('link_marker')
-
-    mark_publisher = rospy.Publisher(
-        '/visualization_marker', Marker, queue_size=10)
-    
-    marker_type = "line"
-    marker = get_marker(marker_type)
+    drawer = MarkerDrawer()
 
     # The time period of the end-efforctor sweeping one circle
     T = rospy.get_param("T", 5.0)
     f = 10.0  # How many points to publish per second.
     rate = rospy.Rate(f)
-    marker.lifetime = rospy.Duration(T*0.6)
+    lifetime = T*0.6
 
     ite = 0
     while not rospy.is_shutdown():
 
+        # Compute the pos of next point,
         m = 2*math.pi * ite / f / T
         x = math.cos(m)
         y = math.sin(m)
 
-        # publish marker
+        # Draw marker
         ite += 1
         if ite > 1:
-            if marker_type == "point":
-                marker.pose.position.x = x
-                marker.pose.position.y = y
-                marker.pose.position.z = 0
-            else:
-                marker.points = [Point(x, y, 0), Point(x_pre, y_pre, 0)]
-            marker.id = ite
-            marker.header.stamp = rospy.Time.now()
-            mark_publisher.publish(marker)
+            drawer.draw_dot(id=ite, 
+                            xyz=[x, y, 0], 
+                            size=0.1,
+                            lifetime=lifetime)
+            drawer.draw_link(id=ite+10000,
+                             xyz1=[x, y, 0],
+                             xyz2=[x_pre, y_pre, 0],
+                             size=0.01,
+                            lifetime=lifetime)
+
+        # Update info.
         x_pre = x
         y_pre = y
 
-        # sleep
-        # rospy.loginfo((x,y))
         rate.sleep()
         if ite % 10 == 0:
             print(ite)
